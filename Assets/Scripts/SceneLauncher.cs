7bdde641df1f83860f152591f5e7e25f0bd0445f
naomi-funki <naomi@funkitron.com>
2020-02-01 16:08:18 -0500

    Set up scene transitions

---
+++ Assets/Scripts/SceneLauncher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLauncher : MonoBehaviour
+{
+    public string scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+   public void TaskOnClick()
+    {
+        Debug.Log(scene);
+        SceneManager.LoadScene(scene);
+    }
+}
