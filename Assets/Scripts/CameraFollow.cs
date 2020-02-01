7537c7e25f6277d719af4335b93894f9f914bd88
chaimaj <chaima.jemmali@gmail.com>
2020-02-01 13:05:10 -0500

    added animations, maze and shadow casting on walls

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float smoothrate;
+
+    Transform player;
+    Vector3 velocity;
+    Vector3 newPos;
+
+    void Start()
+    {
+        velocity = new Vector3(0.5f, 0.5f, 0);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        newPos = Vector3.zero;
+    }
+
+    void FixedUpdate()
+    {
+        newPos.x = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity.x, smoothrate);
+        newPos.y = Mathf.SmoothDamp(transform.position.y, player.position.y, ref velocity.y, smoothrate);
+
+        
+        Vector3 newPosition = new Vector3(newPos.x, newPos.y, -10);
+        transform.position = Vector3.Slerp(transform.position, newPosition, Time.time);
+
+    }
+
+}
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,6 +9,7 @@ public class CharacterMovement : MonoBehaviour
     public Animator animator;
    
     Vector2 movement;
+    float idleTimer = 0;
     // Update is called once per frame
     void Update()
     {
@@ -18,10 +19,22 @@ public class CharacterMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-    }
+        animator.SetFloat("Idle", idleTimer);
 
+
+
+    }
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Movement"))
+        {
+                idleTimer = 0;
+        }
+        else
+        {
+                idleTimer += Time.fixedDeltaTime;
+        }
+       
     }
 }
