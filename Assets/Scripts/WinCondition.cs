a545654a54b16e50a43a3024e65ec3c5e96b7a9f
naomi-funki <naomi@funkitron.com>
2020-02-01 21:53:14 -0500

    Win screen implemented (still needs new animation)

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class WinCondition : MonoBehaviour
 {
+    public GameObject GameOverWinCanvas;
+    public GameObject UICanvas;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             Debug.Log("yeey you win!");
+            GameOverWinCanvas.SetActive(true);
+            UICanvas.SetActive(false);
             Destroy(gameObject);
         }
     }
